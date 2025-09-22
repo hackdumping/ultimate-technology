8a0390806e8909e9998a5f462b7a6209b0817646
hackdumping <erictchoupe5@gmail.com>
2025-09-22 18:44:22 +0100

    Ajout de aiohttp dans requirements.txt

--- UltimateTechnology/settings.py
+++ UltimateTechnology/settings.py
@@ -86,7 +86,7 @@ ROOT_URLCONF = 'UltimateTechnology.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
