49c8ccf2f12f3e4e9026538945aafe75a092024a
hackdumping <erictchoupe5@gmail.com>
2025-09-24 12:32:26 +0100

    mise a jour du projet

--- UltimateTechnology/settings.py
+++ UltimateTechnology/settings.py
@@ -209,6 +209,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 #]
 MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
