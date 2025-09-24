e5f4078775ce809936c472a11950897e6cb0cb90
hackdumping <erictchoupe5@gmail.com>
2025-09-24 12:12:08 +0100

    mise a jour du projet

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-echo "[+] Déploiement en cours..."
+echo "[+]============== Déploiement en cours..."
 
 # Installer les dépendances
-echo "[+] Installation des dépendances..."
+echo "[+]============== Installation des dépendances..."
 python3.12 -m pip install -r requirements.txt
 
-echo "[+] Application des migrations..."
+echo "[+]============== Application des migrations..."
 python3.12 manage.py makemigrations --noinput
 
 # Appliquer les migrations
-echo "[+] Application des migrations..."
+echo "[+]============== Application des migrations..."
 python3.12 manage.py migrate --noinput
 
 # Collecter les fichiers static
-echo "[+] Collecte des fichiers static..."
-python manage.py collectstatic --noinput --clear
+echo "[+]============= Collecte des fichiers static..."
+python3.12 manage.py collectstatic --noinput --clear
 
 echo "✅ Déploiement terminé!"
