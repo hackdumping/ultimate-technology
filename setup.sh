5b9064d43be24dadeb1bea11011c192642a38e23
hackdumping <erictchoupe5@gmail.com>
2025-09-24 06:37:37 +0100

    mise a jour du projet

--- setup.sh
+++ setup.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 echo "[+] Déploiement en cours..."
 
-#vider le cache
-echo "[+] vider le cache pip.."
-pip cache purge
-
 # Installer les dépendances
 echo "[+] Installation des dépendances..."
-pip install setuptools
-pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
+
+echo "[+] Application des migrations..."
+python3.12 manage.py makemigrations --noinput
 
 # Appliquer les migrations
 echo "[+] Application des migrations..."
-python manage.py migrate
+python3.12 manage.py migrate --noinput
 
 # Collecter les fichiers static
 echo "[+] Collecte des fichiers static..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
 echo "✅ Déploiement terminé!"
